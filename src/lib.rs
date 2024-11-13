106ad81b02b3e2933f85b482ba90628e75475175
Mouazzama-dev <mouazzamabinteumer.ep19102044@gmail.com>
2024-11-13 17:15:56 +0500

    with contract_standards

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,94 @@
-use near_sdk::{
-    AccountId, PanicOnDefault, borsh::{BorshDeserialize, BorshSerialize, self},
-    env, json_types::U128, near_bindgen,
+use near_contract_standards::fungible_token::metadata::{
+    FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC,
 };
-use near_sdk_contract_tools::{Owner, ft::*, owner::{*, hooks::OnlyOwner}};
-use near_sdk_contract_tools::standard::nep148::ContractMetadata;
+use near_contract_standards::fungible_token::{
+    FungibleToken, FungibleTokenCore, FungibleTokenResolver,
+};
+use near_contract_standards::storage_management::{
+    StorageBalance, StorageBalanceBounds, StorageManagement,
+};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LazyOption;
+use near_sdk::json_types::U128;
+use near_sdk::{
+    env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, PromiseOrValue, require,
+};
 
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    FungibleToken,
+    Metadata,
+}
 
-#[derive(BorshSerialize, BorshDeserialize, PanicOnDefault, Owner, FungibleToken)]
-#[fungible_token(mint_hook = "OnlyOwner")]
 #[near_bindgen]
-pub struct Contract {}
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract {
+    token: FungibleToken,
+    metadata: LazyOption<FungibleTokenMetadata>,
+    owner_id: AccountId,
+}
 
-// #[near_bindgen]
+#[near_bindgen]
 impl Contract {
-    // #[init]
-    pub fn new() {
-        env::log_str("new method");
-        // let mut contract = Self {};
+    #[init]
+    pub fn new() -> Self {
+        let owner_id = env::predecessor_account_id();
+        let total_supply = U128(1000);
+        let name = "PUSH".to_string();
+        let symbol = "PTB".to_string();
+        let decimals = 5;
 
-        // Owner::init(&mut contract, &env::predecessor_account_id());
-        // Nep148Controller::set_metadata(
-        //     &mut contract,
-        //     &ContractMetadata::new("PUSH".to_string(), "PTB".to_string(), 5),
-        // );
+        let mut token = FungibleToken::new(StorageKey::FungibleToken);
+        token.internal_register_account(&owner_id);
+        token.internal_deposit(&owner_id, total_supply.0);
 
-        // Nep141Controller::mint(
-        //     &mut contract,
-        //     &Nep141Mint {
-        //         amount: 1000u128,
-        //         receiver_id: std::borrow::Cow::Borrowed(&env::predecessor_account_id()),
-        //         memo: None,
-        //     },
-        // )
-        // .unwrap_or_else(|e| env::panic_str(&e.to_string()));
+        let metadata = FungibleTokenMetadata {
+            spec: FT_METADATA_SPEC.to_string(),
+            name,
+            symbol,
+            icon: None,
+            reference: None,
+            reference_hash: None,
+            decimals,
+        };
 
-        // contract
+        let metadata = LazyOption::new(StorageKey::Metadata, Some(&metadata));
+
+        Self { token, metadata, owner_id }
     }
 
     pub fn mint(&mut self, account_id: AccountId, amount: U128) {
-        Nep141Controller::mint(
-            self,
-            &Nep141Mint {
-                amount: amount.into(),
-                receiver_id: std::borrow::Cow::Borrowed(&account_id),
-                memo: None,
-            },
-        )
-        .unwrap_or_else(|e| env::panic_str(&e.to_string()));
+        require!(
+            env::predecessor_account_id() == self.owner_id,
+            "Only the owner can mint tokens"
+        );
+        self.token.internal_deposit(&account_id, amount.0);
     }
 
     pub fn burn(&mut self, amount: U128) {
-        Nep141Controller::burn(
-            self,
-            &Nep141Burn {
-                amount: amount.into(),
-                owner_id: std::borrow::Cow::Borrowed(&env::predecessor_account_id()),
-                memo: None,
-            },
-        )
-        .unwrap_or_else(|e| env::panic_str(&e.to_string()));
+        let account_id = env::predecessor_account_id();
+        self.token.internal_withdraw(&account_id, amount.0);
+    }
+}
+
+// #[near_bindgen]
+// impl FungibleTokenCore for Contract {
+//     // Implement required methods for FungibleTokenCore
+// }
+
+// #[near_bindgen]
+// impl FungibleTokenResolver for Contract {
+//     // Implement required methods for FungibleTokenResolver
+// }
+
+// #[near_bindgen]
+// impl StorageManagement for Contract {
+//     // Implement required methods for StorageManagement
+// }
+
+#[near_bindgen]
+impl FungibleTokenMetadataProvider for Contract {
+    fn ft_metadata(&self) -> FungibleTokenMetadata {
+        self.metadata.get().unwrap()
     }
 }
