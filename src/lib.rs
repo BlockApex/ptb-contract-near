d1c208062994207f15e4bd81b8903dbd0de98a5b
Mouazzama-dev <mouazzamabinteumer.ep19102044@gmail.com>
2024-11-14 17:56:33 +0500

    initialziation updated accoridng to the pools

--- src/lib.rs
+++ src/lib.rs
@@ -8,20 +8,49 @@ use near_contract_standards::storage_management::{
     StorageBalance, StorageBalanceBounds, StorageManagement,
 };
 use near_sdk::borsh::BorshSerialize;
+use near_sdk::borsh::BorshDeserialize;
 use near_sdk::collections::LazyOption;
 use near_sdk::json_types::U128;
 use near_sdk::{
     env, log, near, require, AccountId, BorshStorageKey, NearToken, PanicOnDefault, PromiseOrValue,
 };
+use near_sdk::collections::{LookupMap};
+
+#[derive(BorshSerialize, BorshDeserialize, PanicOnDefault)]
+pub struct EmissionsAccount {
+    pub initial_emissions: u64,
+    pub decay_factor: f64,
+    pub current_month: u32,
+    pub current_emissions: u64,
+    pub last_mint_timestamp: u64,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, PanicOnDefault)]
+pub struct RafflePool {
+    pub pool_id: u32,
+    pub amount: u128,
+    pub total_amount: u128,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct TappingPool {
+    pub pool_id: u32,
+    pub amount: u128,
+}
+
 
 #[derive(PanicOnDefault)]
 #[near(contract_state)]
 pub struct Contract {
     token: FungibleToken,
     metadata: LazyOption<FungibleTokenMetadata>,
+    emissions_account: LookupMap<AccountId, EmissionsAccount>,
+    loot_raffle_pool: LookupMap<u32, RafflePool>,
+    global_tapping_pool: LookupMap<u32, TappingPool>,
+
 }
 
-const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='https://red-defensive-termite-556.mypinata.cloud/ipfs/QmUCUAABBsqkhSw3HoeMtecwVAeKBmxUgj2GLwmxuNojbV' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 
 #[derive(BorshSerialize, BorshStorageKey)]
 #[borsh(crate = "near_sdk::borsh")]
@@ -36,19 +65,48 @@ impl Contract {
     /// default metadata (for example purposes only).
     #[init]
     pub fn new_default_meta(owner_id: AccountId, total_supply: U128) -> Self {
-        Self::new(
-            owner_id,
-            total_supply,
-            FungibleTokenMetadata {
+        require!(!env::state_exists(), "Already initialized");
+        
+        let mut this = Self {
+            token: FungibleToken::new(StorageKey::FungibleToken),
+            metadata: LazyOption::new(StorageKey::Metadata, Some(&FungibleTokenMetadata {
                 spec: FT_METADATA_SPEC.to_string(),
-                name: "Example NEAR fungible token".to_string(),
-                symbol: "EXAMPLE".to_string(),
+                name: "PUSH THE BUTTON PTB".to_string(),
+                symbol: "PUSH".to_string(),
                 icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
                 reference: None,
                 reference_hash: None,
-                decimals: 24,
-            },
-        )
+                decimals: 5,
+            })),
+            emissions_account: LookupMap::new(b"e"),
+            loot_raffle_pool: LookupMap::new(b"l"),
+            global_tapping_pool: LookupMap::new(b"g"),
+        };
+
+        // Initialize Emissions Account
+        this.emissions_account.insert(&owner_id, &EmissionsAccount {
+            initial_emissions: 3_000_000_000,
+            decay_factor: 0.8705505633,
+            current_month: 0,
+            current_emissions: 3_000_000_000,
+            last_mint_timestamp: env::block_timestamp(),
+        });
+
+        // Initialize Raffle and Tapping Pools
+        this.loot_raffle_pool.insert(&1, &RafflePool {
+            pool_id: 1,
+            amount: 50_000_000_00000,
+            total_amount: 0,
+        });
+        this.global_tapping_pool.insert(&2, &TappingPool {
+            pool_id: 2,
+            amount: 1_000_000_000_00000,
+        });
+
+        this.token.internal_register_account(&owner_id);
+        this.token.internal_deposit(&owner_id, total_supply.into());
+
+        this
     }
 
     /// Initializes the contract with the given total supply owned by the given `owner_id` with
@@ -60,6 +118,10 @@ impl Contract {
         let mut this = Self {
             token: FungibleToken::new(StorageKey::FungibleToken),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            emissions_account: LookupMap::new(b"e"),
+            loot_raffle_pool: LookupMap::new(b"l"),
+            global_tapping_pool: LookupMap::new(b"g"),
+
         };
         this.token.internal_register_account(&owner_id);
         this.token.internal_deposit(&owner_id, total_supply.into());
@@ -74,17 +136,17 @@ impl Contract {
         this
     }
 
-        // Mint function added here
-        pub fn mint(&mut self, account_id: AccountId, amount: U128) {
-            self.token.internal_deposit(&account_id, amount.into());
+    // Mint function added here
+    pub fn mint(&mut self, owner_id: AccountId, amount: U128) {
+        self.token.internal_deposit(&owner_id, amount.into());
     
-            near_contract_standards::fungible_token::events::FtMint {
-                owner_id: &account_id,
-                amount: amount,
-                memo: Some("tokens minted"),
-            }
-            .emit();
+        near_contract_standards::fungible_token::events::FtMint {
+        owner_id: &owner_id,
+        amount: amount,
+        memo: Some("tokens minted"),
         }
+        .emit();
+    }
     
     
 }
